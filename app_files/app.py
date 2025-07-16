95df6d71f778c2703fb617f15557d0b9e510fa09
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-07-16 18:52:10 +0300

    Update app.py

--- app_files/app.py
+++ app_files/app.py
@@ -3,7 +3,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        message = "Hello from Backend"
+        message = "Hello from Backend \n   This is a simple HTTP server. \n  You can access it at http://localhost:8080"
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
@@ -12,5 +12,5 @@ class SimpleHandler(BaseHTTPRequestHandler):
 if __name__ == "__main__":
     server_address = ("", 8080) # Use port 8080
     httpd = HTTPServer(server_address, SimpleHandler)
-    print("Server running on port 80...")
+    print("Server running on port 8080...")
     httpd.serve_forever()
