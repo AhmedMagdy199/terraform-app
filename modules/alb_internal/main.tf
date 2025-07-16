feda34eb041b5ccce06e5b2a47c26ae8af8cff13
Ahmed Magdy <104032428+AhmedMagdy199@users.noreply.github.com>
2025-07-16 18:54:22 +0300

    Update main.tf

--- modules/alb_internal/main.tf
+++ modules/alb_internal/main.tf
@@ -32,7 +32,7 @@ resource "aws_lb_target_group" "backend_tg" {
   }
 }
 
-resource "aws_lb_listener" "http" {
+resource "aws_lb_listener" "internal_http" {
   load_balancer_arn = aws_lb.internal_alb.arn
   port              = 80
   protocol          = "HTTP"
